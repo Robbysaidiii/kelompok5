85ddb7ea9568a1d951b6c0f5844cd2ed0a92019a
Robby Saidi Prasetyo <157108492+Robbysaidiii@users.noreply.github.com>
2024-12-14 19:19:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def predict_rain():
 # Route to render the index.html page
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('base.html')
 
 # Run the Flask app
 if __name__ == '__main__':
